2bae131580153141c09175c5c88039f2a1d64bd6
unknown <teddytiyi@gmail.com>
2023-07-26 20:57:59 +0200

    Add classroom.rb

---
+++ classroom.rb
@@ -0,0 +1,15 @@
+require_relative 'student'
+
+class Classroom
+  attr_accessor :label, :students
+
+  def initialize(label)
+    @label = label
+    @students = []
+  end
+
+  def add_student(student)
+    student.classroom = self
+    @students << student
+  end
+end
