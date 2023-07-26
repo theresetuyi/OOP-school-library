7c7757da0f29872f0cef518737555a61843787ea
unknown <teddytiyi@gmail.com>
2023-07-26 20:59:27 +0200

    modified person and student file

--- main.rb
+++ main.rb
@@ -1,13 +1,24 @@
 require_relative 'person'
-require_relative 'decorator'
-require_relative 'capitalize_decorator'
-require_relative 'trimmer_decorator'
+require_relative 'book'
+require_relative 'rental'
+require_relative 'classroom'
 
-person = Person.new(name: 'Maximilianus', age: 22)
-puts person.correct_name
+# Creating persons
+person1 = Person.new(name: 'John', age: 25)
+person2 = Person.new(name: 'Alice', age: 17, parent_permission: false)
 
-capitalized_person = CapitalizeDecorator.new(person)
-puts capitalized_person.correct_name
+# Creating books
+book1 = Book.new('Book A', 'Author A')
+book2 = Book.new('Book B', 'Author B')
 
-capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
-puts capitalized_trimmed_person.correct_name
+# Creating rentals and establishing relationships
+Rental.new(person1, book1, '2023-07-26')
+Rental.new(person1, book2, '2023-07-27')
+Rental.new(person2, book1, '2023-07-28')
+
+# Accessing rentals for a person and a book
+puts "#{person1.name}'s rentals:"
+person1.rentals.each { |rental| puts "- #{rental.book.title} (#{rental.date})" }
+
+puts "#{book1.title}'s rentals:"
+book1.rentals.each { |rental| puts "- #{rental.person.name} (#{rental.date})" }
--- student.rb
+++ student.rb
@@ -1,4 +1,5 @@
 require_relative 'person'
+require_relative 'classroom'
 
 class Student < Person
   attr_accessor :classroom
@@ -6,6 +7,7 @@ class Student < Person
   def initialize(classroom:, name: 'Unknown', age: nil, parent_permission: true)
     super(name: name, age: age, parent_permission: parent_permission)
     @classroom = classroom
+    classroom&.add_student(self) # Use safe navigation (&.) here
   end
 
   def play_hooky
